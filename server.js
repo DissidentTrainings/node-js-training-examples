5c53a8c8696a3cb8d90c66de4c9fcbc81332a2a6
Sebastian Schürmann <sebs@2xs.org>
2014-11-19 12:04:41 +0100

    Added event

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
 
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(index);
 }).listen(9615);
+
+
+server.on('connection', function() {
+ console.log('connect');
+});
