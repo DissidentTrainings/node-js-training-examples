d06192d1f87b57fcf3de50a6252be6d05031fdf8
Sebastian Schürmann <sebs@2xs.org>
2014-11-19 12:40:43 +0100

    Using env variables for the debugging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var index = fs.readFileSync('index.html');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(index);
-}).listen(9615);
+}).listen(process.env.PORT, process.env.IP);
 
 
 server.on('connection', function() {
